1d32a6394a2548d2169cdd30a173e3a68c7fbd7f
Kate P <katep@unknown5855caf7f2eb.gateway.2wire.net>
2014-07-11 20:26:15 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,20 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild: character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    //adds obstacle every 2 seconds
+    if (timeSinceObstacle > 2.0f)
+    {
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
